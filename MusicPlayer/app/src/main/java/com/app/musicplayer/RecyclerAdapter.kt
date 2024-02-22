6d317c8f9ea48c3bb2c2765904061585ed0c9bf4
Pol <Tihersarc35@gmail.com>
2024-02-22 16:10:25 +0100

    App working

--- MusicPlayer/app/src/main/java/com/app/musicplayer/RecyclerAdapter.kt
+++ MusicPlayer/app/src/main/java/com/app/musicplayer/RecyclerAdapter.kt
@@ -1,11 +1,15 @@
 package com.app.musicplayer
 
 import android.annotation.SuppressLint
+import android.content.Context
+import android.media.MediaPlayer
+import android.net.Uri
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.AdapterView
+import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
@@ -15,6 +19,7 @@ import com.squareup.picasso.Picasso
 class RecyclerAdapter(private var mSongs: MutableList<Song> = mutableListOf()) : RecyclerView.Adapter<RecyclerAdapter.ViewHolder>() {
 
     private var onItemClickListener: AdapterView.OnItemClickListener? = null
+    private var isMediaPlayerPrepared = false
 
     fun setOnItemClickListener(listener: AdapterView.OnItemClickListener) {
         this.onItemClickListener = listener
@@ -23,11 +28,14 @@ class RecyclerAdapter(private var mSongs: MutableList<Song> = mutableListOf()) :
     inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         val songImage: ImageView = itemView.findViewById(R.id.image)
         val songName: TextView = itemView.findViewById(R.id.name)
-
+        val playButton : Button = itemView.findViewById(R.id.playButton)
+        val pauseButton : Button = itemView.findViewById(R.id.pauseButton)
+        val stopButton : Button = itemView.findViewById(R.id.stopButton)
     }
 
+    private lateinit var context : Context;
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val context = parent.context
+        context = parent.context
         val inflater = LayoutInflater.from(context)
         val songView = inflater.inflate(R.layout.song_container, parent, false)
 
@@ -39,8 +47,9 @@ class RecyclerAdapter(private var mSongs: MutableList<Song> = mutableListOf()) :
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        val song: Song = mSongs[position]
+        var mediaPlayer: MediaPlayer? = null
 
+        val song: Song = mSongs[position]
         val imageUrl = song.image
 
         Picasso.get()
@@ -49,7 +58,6 @@ class RecyclerAdapter(private var mSongs: MutableList<Song> = mutableListOf()) :
                 override fun onSuccess() {
                     Log.d("Picasso", "Image loaded successfully for item: ${song.name}")
                 }
-
                 override fun onError(e: Exception?) {
                     e?.printStackTrace()
                     Log.e("Picasso", "Error loading image for item: ${song.name}", e)
@@ -57,6 +65,36 @@ class RecyclerAdapter(private var mSongs: MutableList<Song> = mutableListOf()) :
             })
 
         holder.songName.text = song.name
+
+        if (song.mediaPlayer == null) {
+            song.mediaPlayer = MediaPlayer.create(context, song.path)
+        }
+
+        holder.playButton.setOnClickListener {
+            val mediaPlayer = song.mediaPlayer
+            mediaPlayer?.apply {
+                if (!isPlaying) {
+                    start()
+                }
+            }
+        }
+
+        holder.pauseButton.setOnClickListener {
+            val mediaPlayer = song.mediaPlayer
+            mediaPlayer?.apply {
+                if (isPlaying) {
+                    pause()
+                }
+            }
+        }
+
+        holder.stopButton.setOnClickListener {
+            val mediaPlayer = song.mediaPlayer
+            mediaPlayer?.apply {
+                stop()
+                prepareAsync()
+            }
+        }
     }
 
     @SuppressLint("NotifyDataSetChanged")
--- MusicPlayer/app/src/main/java/com/app/musicplayer/Song.kt
+++ MusicPlayer/app/src/main/java/com/app/musicplayer/Song.kt
@@ -1,7 +1,10 @@
 package com.app.musicplayer
 
+import android.media.MediaPlayer
+
 data class Song (
     var name : String,
     var path : Int,
-    var image : String
+    var image : String,
+    var mediaPlayer: MediaPlayer? = null
 )
